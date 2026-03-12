17ffbad0208b932492f13fd8f4b12ffc004e4725
SandraBlu <blu11@me.com>
2026-03-11 23:07:09 -0700

    add source files

---
+++ Source/AlphaOmega/Private/Actor/EffectActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/EffectActor.h"
+
+// Sets default values
+AEffectActor::AEffectActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEffectActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
